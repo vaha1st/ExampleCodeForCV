b7a3b831b2663c047e85155339b576d2a8842196
Ruslan <vaha1st@gmail.com>
2020-04-18 20:56:51 +0300

    Work with documentation. Update readme. Version 1.1 - basic Web form support

--- src/com/vaha1st/controllers/TemperatureController.java
+++ src/com/vaha1st/controllers/TemperatureController.java
@@ -12,6 +12,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.validation.Valid;
 
+/**
+ * {@code TemperatureController} контроллер для обработки запросов по работе с температурным конвертером.
+ *
+ * @author Руслан Вахитов
+ * @version 1.00 17 Apr 2020
+ */
+
 @Controller
 @RequestMapping("/temperature")
 public class TemperatureController {
@@ -19,18 +26,22 @@ public class TemperatureController {
     // Создание spring контейнера с помощью java конфигурационного класса
     AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SpringConfig.class);
 
+    // Обработка запросов на ввод данных.
     @RequestMapping("/input")
     private String input(Model model) {
 
+        // Получение бина простейшей конвертации.
         SimpleValuesInput input = context.getBean("simpleValuesInput", SimpleValuesInput.class);
 
+        // Добавление в атрибуты модели полученного бина и значений ENUM TemperatureUnits
         model.addAttribute("input", input);
-
         model.addAttribute("temperatureUnits", TemperatureUnits.values());
 
+        // Адресация к temperature-converter.jsp
         return "temperature-converter";
     }
 
+    // Обработка введенных данных из формы в temperature-converter.jsp
     @RequestMapping("/result")
     private String processConversion(
             @Valid @ModelAttribute("input") SimpleValuesInput input, BindingResult bindingResult) {
